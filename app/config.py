5d964262105d8ac4d83435b2a830efd267fd59ff
abhirockraj <abhinavkool19@gmail.com>
2022-06-26 15:47:46 +0530

    changed address

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgres://nxpzkbvfdjmqta:4fcb0a4aff18e26977938ad6bdc84e7be4a7b36b4039d447437a6aa936bf4af8@ec2-52-71-23-11.compute-1.amazonaws.com:5432/d100esmnuvhotb'
+DATABASE_URL = 'postgres://lfdyhtjcioqgzc:2d8acb8e87b0cc54ed811327506370de7d7467b7e3ceecf2a2d27eda76e84c25@ec2-44-197-128-108.compute-1.amazonaws.com:5432/d4j9u52qg04vk5'
 
 engine = create_engine(DATABASE_URL,echo= False)
 SessionLocal = sessionmaker(autocommit=False, autoflush = False, bind=engine)
